9d849e5e26725d13a8ae164bf499599df7d36590
Remco <remco@yosora.com>
2024-02-23 23:29:37 +0100

    Update

---
+++ Scripts/Plexamp NFC reader/README.md
@@ -0,0 +1,165 @@
+# NFC cardreader for (headless) Plexamp on a Raspberry Pi
+A python based script to read the Plexamp library URL of a NFC tag or card.
+
+## Workings
+NFC cards can be writen from within the Plexamp mobile app. This will write the library or playlist URL to the NFC card. This script will read the data of the NFC card and converts it back to a URL. That URl is then openend on the system the system the script runs on.
+
+## Prerequisites
+This code is tested, and meant to be used with:
+
+-  a Mifare Classic 1K card/tag
+- a [Elechouse PN532 V3 NFC/RFID card reader](https://aliexpress.com/item/1005005973913526.html)
+
+------------
+
+##Installation of PN532 hardware on Raspberry Pi
+The PN532 has 3 connection modes, SPI, HSU and I2C. For this usecase the I2C connection is exclusively used by the IQaudio DigiAMP+ and therefore not avaible for use with the PN532. Luckily there are 2 other ways, SPI and HSU:
+
+###...in SPI mode (recommended)
+
+Install dependent packages:
+```bash
+sudo apt-get update
+sudo apt-get  install  libpcsclite-dev  -y
+```
+
+Download and unzip the source code package of libnfc
+```bash
+cd ~
+wget https://github.com/nfc-tools/libnfc/releases/download/libnfc-1.8.0/libnfc-1.8.0.tar.bz2
+tar -xf libnfc-1.7.1.tar.bz2
+```
+
+Compile and install libnfc.
+```bash
+cd libnfc-1.7.1
+./configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+```
+
+Open SPI interface
+```bash
+sudo raspi-config
+```
+Interface Options -> I4 SPI -> Yes -> Ok -> Finish
+
+Write the configuration file for NFC communication
+```bash
+sudo mkdir /etc/nfc
+cd /etc/nfc
+sudo nano libnfc.conf
+```
+
+Add:
+```bash
+# Allow device auto-detection (default: true)
+# Note: if this auto-detection is disabled, user has to set manually a device
+# configuration using file or environment variable
+allow_autoscan = true
+
+# Allow intrusive auto-detection (default: false)
+# Warning: intrusive auto-detection can seriously disturb other devices
+# This option is not recommended, user should prefer to add manually his device.
+allow_intrusive_scan = false
+
+# Set log level (default: error)
+# Valid log levels are (in order of verbosity): 0 (none), 1 (error), 2 (info), 3 (debug)
+# Note: if you compiled with --enable-debug option, the default log level is "debug"
+log_level = 1
+
+# Manually set default device (no default)
+# To set a default device, you must set both name and connstring for your device
+# Note: if autoscan is enabled, default device will be the first device available in device list.
+device.name = "_PN532_SPI"
+device.connstring = "pn532_spi:/dev/spidev0.0:280000"
+```
+
+**WIRING**
+Toggle the switch to the SPI mode
+
+Connect the PN532:
+| PN532 | Raspberry Pi |
+| ------------ | ------------ |
+| VCC | 3.3V |
+| SCK | SCKL |
+| MISO | MISO |
+| MOSI | MOSI |
+| SS | CE0 |
+
+**Testing**
+Check whether the SPI is opened or not:
+```bash
+ls   /dev/spidev0.*
+```
+
+Check if NFC module is found
+```bash
+nfc-list
+```
+
+Run nfc-poll to scan the RFID tag and you can read information on the card
+```bash
+nfc-poll
+```
+When you can do a succesfull nfc-poll you can continue with [installing the pn532pi library]()
+
+source: https://osoyoo.com/2017/07/20/pn532-nfc-rfid-module-for-raspberry-pi/
+
+
+###...in HSU mode
+
+Install dependent packages:
+```bash
+sudo apt-get update
+sudo apt-get  install git autoconf libtool libusb-dev  -y
+```
+
+Download the source code package of libnfc
+```bash
+cd ~
+git clone https://github.com/nfc-tools/libnfc
+```
+
+Write the configuration file for NFC communication
+```bash
+sudo mkdir -p /etc/nfc/devices.d
+cd libnfc
+sudo cp contrib/libnfc/pn532_uart_on_rpi_3.conf.sample /etc/nfc/devices.d/pn532_uart_on_rpi_3.conf
+```
+
+Compile and install libnfc.
+```bash
+autoreconf -vis
+./configure --with-drivers=pn532_uart --sysconfdir=/etc --prefix=/usr
+make
+sudo make install all
+```
+
+**Wiring**
+Toggle the switch to the HSU mode
+
+Connect the PN532:
+| PN532 | Raspberry Pi |
+| ------------ | ------------ |
+| VCC | 3.3V |
+| GND | GND |
+| TXD | GPIO14 (TXD0) |
+| RXD | GPIO15 (RXD0) |
+
+
+**Testing**
+Check if NFC module is found
+```bash
+nfc-list
+```
+
+Run nfc-poll to scan the RFID tag and you can read information on the card
+```bash
+nfc-poll
+```
+pn53x_check_communication error: Try swapping the RX and TX wires
+
+When you can do a succesfull nfc-poll you can continue with [installing the pn532pi library]()
+
+source: https://cdn-learn.adafruit.com/downloads/pdf/adafruit-nfc-rfid-on-raspberry-pi.pdf
---
+++ Scripts/Plexamp NFC reader/plexamp-nfc-reader.py
@@ -0,0 +1,122 @@
+"""
+    This code is tested, and meant to be used with:
+    - a Mifare Classic 1K card
+    - a Elechouse PN532 V3 NFC/RFID card reader
+
+    The NFC card can be writen with an (android) phone to contain a
+    plexamp playlist URL. This code attempts to read the contents of 
+    the NFC card and convert is back to a URL and open the URL.
+
+    Note that you need the baud rate to be 115200 because we need to print
+    out the data and read from the card at the same time!
+
+    This code is a modified version of https://github.com/gassajor000/pn532pi/blob/master/examples/mifareclassic_memdump.py
+"""
+
+import curlify
+import requests
+import time
+
+from pn532pi import pn532, Pn532
+from pn532pi import Pn532Hsu
+from pn532pi import Pn532Spi
+
+SPI = True
+HSU = False
+
+if SPI:
+    PN532_SPI = Pn532Spi(Pn532Spi.SS0_GPIO8)
+    nfc = Pn532(PN532_SPI)
+# When the number after #elif set as 1, it will be switch to HSU Mode
+elif HSU:
+    PN532_HSU = Pn532Hsu(Pn532Hsu.RPI_MINI_UART)
+    nfc = Pn532(PN532_HSU)
+
+def setup():
+  nfc.begin()
+
+  versiondata = nfc.getFirmwareVersion()
+  if not versiondata:
+    raise RuntimeError("Didn't find PN53x board")  # halt
+
+  # configure board to read RFID tags
+  nfc.SAMConfig()      
+
+
+if __name__ == '__main__':
+    setup()
+    while True:
+      authenticated = False               # Flag to indicate if the sector is authenticated
+
+      # Keyb on NDEF and Mifare Classic should be the same
+      keyuniversal = bytearray([ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF ])
+      # Set an empty data string for all the data to collect in
+      data_string = 'http://'
+
+      # Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+      # if the uid is 4 bytes (Mifare Classic)
+      success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS)
+
+      if (success):
+        if (len(uid) == 4):
+
+          # Set blockcounter to skip every 4th block
+          blockcount = 0
+          # Stop processing when 4 consecutive empty blocks are found
+          emptyblockcount = 0
+
+          # Now we try to go through all 16 sectors (each having 4 blocks)
+          for currentblock in range(64):
+            if (blockcount == 3):
+              # Reset counter and skip
+              blockcount = 0
+            else:
+              # Add to blockcount and proceed
+              blockcount += 1
+
+              # Check if this is a new block so that we can reauthenticate
+              if (nfc.mifareclassic_IsFirstBlock(currentblock)):
+                authenticated = False
+
+              # Keep trying to authenticated the block until authentication succeeded
+              while (not authenticated and currentblock > 3):
+                success = nfc.mifareclassic_AuthenticateBlock (uid, currentblock, 1, keyuniversal)
+                if (success):
+                  authenticated = True       
+                
+              if (authenticated):
+                # Authenticated ... we should be able to read the block now
+                # Skip reading empty blocks 
+                emptyblock = bytearray([ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ])
+                
+                # Dump the data into the 'data' array
+                success, data = nfc.mifareclassic_ReadDataBlock(currentblock)
+
+                if (data == emptyblock):
+                  # If block is empty add to counter
+                  emptyblockcount += 1
+                  if (emptyblockcount > 3):
+                    # 4 empty blocks in a row, break from the loop
+                    break
+                else:
+                  if (success):
+                    # Block contains data, reset emptyblockcount
+                    emptyblockcount = 0
+                    # Read successful
+                    if (currentblock == 4):
+                      # On the 4th block (start of our data), remove the first 7 characters
+                      data = data[7:]
+                    # convert data to string
+                    string_object = data.decode('utf8', 'ignore')
+                    # Add string to the other strings
+                    data_string = data_string + string_object
+
+        # Replace listen.plex.tv for localhost so the link will open on the local device
+        data_string = data_string.replace("listen.plex.tv", "localhost:32500")
+
+        # Open the link
+        response = requests.get(data_string)
+        print(curlify.to_curl(response.request))
+
+        # Sleep for 3 seconds
+        time.sleep(3)
