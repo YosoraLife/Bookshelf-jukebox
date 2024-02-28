e2a9ac60449efb850d09fee2171e115e63e856fc
Remco <remco@yosora.com>
2024-02-28 15:59:11 +0100

    Updated startup script to use thorium

--- plexamp-startup.sh
+++ plexamp-startup.sh
@@ -9,13 +9,14 @@ xset -dpms
 unclutter -root &
 
 # Subpress the warning baar
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/thorium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/thorium/Default/Preferences
 
 # Open a single tab
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:32500/ &
+/usr/bin/thorium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:32500/ &
 
-
+python3 /home/pi/plexamp-jukebox/plexamp-controls.py &
+python3 /home/pi/plexamp-jukebox/plexamp-nfc-reader.py &
 
 # Allow switching between tabs on buttonpress
 # /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:32500/ http://localhost/settings &
