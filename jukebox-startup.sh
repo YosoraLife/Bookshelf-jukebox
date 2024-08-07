9b3d725c39af3b93c93a98d21a37952b0c174815
Remco <remco@yosora.com>
2024-08-07 12:39:06 +0200

    Updated startup script

--- jukebox-startup.sh
+++ jukebox-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/python3 /home/$USER/bookshelf-jukebox/controls.py &
-/usr/bin/python3 /home/$USER/bookshelf-jukebox/screen.py &
-/usr/bin/python3 /home/$USER/bookshelf-jukebox/nfc_reader.py &
+/usr/bin/python3 /root/bookshelf-jukebox/controls.py &
+/usr/bin/python3 /root/bookshelf-jukebox/screen.py &
+/usr/bin/python3 /root/bookshelf-jukebox/nfc_reader.py &
